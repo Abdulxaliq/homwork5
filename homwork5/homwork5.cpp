2ed504f1e7908d48017d052c1f69d17f5646e735
Abdulxaliq <Agaye_ck17@itstep.edu.az>
2024-07-16 16:00:16 +0400

    Add project files.

---
+++ homwork5/homwork5.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//double gb = 0;
+	//double bit_san = 0;
+
+	//cout << "gb: ";
+	//cin >> gb;
+	//cout << "bit_san: ";
+	//cin >> bit_san;
+	//double gb_bit = gb * 8000000000;
+
+
+	//double gb_2 = 0;
+	//cout << "gb_2: ";
+	//cin >> gb_2;
+	//int gb_2_mb = gb_2 * 1000;
+	//int film = gb_2_mb / 760;
+	//cout << "Sizin filminiz " << film << " defe yerlesir";
+
+
+
+	//double verebilen = 0;
+	//double verebilmeyen = 0;
+
+	//cout << "verebilen: ";
+	//cin >> verebilen;
+	//cout << "verebilmeyen: ";
+	//cin >> verebilmeyen;
+
+	//double umumi = verebilen + verebilmeyen;
+	//int verebilen_faiz = verebilen / umumi * 100;
+	//int verebilmeyen_faiz = verebilmeyen / umumi * 100;
+	//cout << "Imtahani verebilenlerin faizi: " << verebilen_faiz << "%";
+	//cout << "Imtahani verebilmeyenlerin faizi: " << verebilmeyen_faiz << "%";
+
+
+
+	//double saniye = 0;
+
+	//cout << "saniye: ";
+	//cin >> saniye;
+
+	//double g_sonu_saniye = 86400 - saniye;
+	//double g_sonu_saat = g_sonu_saniye / 3600;
+	//double g_sonu_deq = g_sonu_saniye / 60;
+	//cout << "Gunun sonuna qalan saat: " << g_sonu_saat << endl;
+	//cout << "Gunun sonuna qalan deqiqe: " << g_sonu_deq << endl;
+	//cout << "Gunun sonuna qalan saniye: " << g_sonu_saniye ;
+
+
+	//double gigabayt = 0;
+	//cout << "gigabayt: ";
+	//cin >> gigabayt;
+	//double mb = gigabayt * 1000;
+	//int disk = mb / 1.44;
+	//cout << "Size lazim olan disket: " << disk;
+
+
+	//double kesr_eded = 0;
+	//cout << "kesr_eded: ";
+	//cin >> kesr_eded;
+	//float yuvarlaqlasdirma = (int)(kesr_eded - (int)kesr_eded) % 100;
+	//double yuzdebir = kesr_eded - yuvarlaqlasdirma;
+	//cout << "yuvarlasdirdiqda aldiqimiz eded: " << yuzdebir;
+}
